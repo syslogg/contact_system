80e18c51b029890032cecc812b86eda98f56f446
Hendy Rodrigues F. Silva <mr.syslogg@gmail.com>
2017-09-21 06:57:38 -0300

    Implementado funções da UnitOfWork

---
+++ Repository/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using Repository.Interfaces.Repositories;
+using System;
+
+namespace Repository.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IPersonRepository PersonRepository { get; }
+        IAddressRepository AddressRepository { get; }
+        IContactRepository ContactRepository { get; }
+
+        void Commit();
+        Task CommitAsync();
+    }
+}
---
+++ Repository/UnitOfWork.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Repository.Interfaces;
+using Repository.Interfaces.Repositories;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        public IPersonRepository PersonRepository { get; private set; }
+
+        public IAddressRepository AddressRepository { get; private set; }
+
+        public IContactRepository ContactRepository { get; private set; }
+
+        private DbContactSystemContext _dbContext;
+
+        public UnitOfWork()
+        {
+            CreateDbContext();
+        }
+
+        public void Commit()
+        {
+            _dbContext.SaveChanges();
+        }
+
+        public async Task CommitAsync()
+        {
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void CreateDbContext()
+        {
+            _dbContext = new DbContactSystemContext();
+        }
+
+    }
+}

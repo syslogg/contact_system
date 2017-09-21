1ec9f222ad276b310acd6bb6e07d0445435cc7e3
Hendy Rodrigues F. Silva <mr.syslogg@gmail.com>
2017-09-21 01:07:09 -0300

    Criado arquivo Repository.cs contendo repositorio com operações basicas de todas entidades

--- Repository/Base/Repository.cs
+++ Repository/Base/Repository.cs
@@ -1,39 +1,102 @@
 ﻿using System;
 using System.Collections.Generic;
 using Repository.Interfaces;
+using System.Data.Entity;
+using Repository.Exceptions;
+using System.Linq;
+using System.Data.Entity.Infrastructure;
 
 namespace Repository.Base
 {
-    public class Repository<T> : IRepository<T>
+    public class Repository<T> : IRepository<T> where T : class
     {
+        private readonly DbContactSystemContext _dbContext;
+        private IDbSet<T> _dbSet;
+        
+        public Repository(DbContactSystemContext dbContext)
+        {
+            this._dbContext = dbContext;
+            this._dbSet = _dbContext.Set<T>();
+        }
+
         public T Add(T entity)
         {
-            throw new NotImplementedException();
+            try
+            {
+                return this._dbSet.Add(entity);
+            } catch (Exception re)
+            {
+                throw new RepositoryException(re.Message, re);
+            }
         }
 
         public void Delete(T entity)
         {
-            throw new NotImplementedException();
+            try
+            {
+                _dbSet.Attach(entity);
+                _dbSet.Remove(entity);
+            }
+            catch (Exception e)
+            {
+                throw new RepositoryException(e.Message);
+            }
         }
 
         public List<T> Find(Func<T, bool> predicate)
         {
-            throw new NotImplementedException();
+            try
+            {
+                return _dbSet.Where(predicate).ToList();
+            }
+            catch (Exception e)
+            {
+                throw new RepositoryException(e.Message, e);
+            }
         }
 
         public T Get(int id)
         {
-            throw new NotImplementedException();
+            try
+            {
+                return _dbSet.Find(id);
+            }
+            catch (Exception e)
+            {
+                throw new RepositoryException(e.Message, e);
+            }
         }
 
         public IEnumerable<T> ListAll()
         {
-            throw new NotImplementedException();
+            try
+            {
+                return _dbSet;
+            }
+            catch (Exception e)
+            {
+                throw new RepositoryException(e.Message,e);
+            }
         }
 
         public bool Update(T entity)
         {
-            throw new NotImplementedException();
+            try
+            {
+                DbEntityEntry dbEntityEntry = _dbContext.Entry(entity);
+
+                if (dbEntityEntry.State == EntityState.Detached)
+                {
+                    _dbSet.Attach(entity);
+                }
+
+                dbEntityEntry.State = EntityState.Modified;
+                return true;
+            }
+            catch (Exception e)
+            {
+                throw new RepositoryException(e.Message);
+            }
         }
     }
 }
---
+++ Repository/Exceptions/RepositoryException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Repository.Exceptions
+{
+    public class RepositoryException : Exception
+    {
+        public RepositoryException() 
+            : base() { }
+
+        public RepositoryException(string msg) 
+            : base(msg) { }
+
+        public RepositoryException(string msg, Exception innerException) 
+            : base(msg,innerException)
+        { }
+    }
+}

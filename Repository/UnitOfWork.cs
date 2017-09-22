f31374c30002ba2cc5e84c02d85c98e9022180b3
Hendy Rodrigues F. Silva <mr.syslogg@gmail.com>
2017-09-22 13:29:29 -0300

    Desenvolvido UnitOfWOrk

--- Repository/UnitOfWork.cs
+++ Repository/UnitOfWork.cs
@@ -2,22 +2,69 @@
 using Repository.Interfaces;
 using Repository.Interfaces.Repositories;
 using System.Threading.Tasks;
+using Repository.Repositories;
 
 namespace Repository
 {
     public class UnitOfWork : IUnitOfWork
     {
-        public IPersonRepository PersonRepository { get; private set; }
 
-        public IAddressRepository AddressRepository { get; private set; }
+        private IPersonRepository _personRepository = null;
 
-        public IContactRepository ContactRepository { get; private set; }
+        public IPersonRepository PersonRepository
+        {
+            get
+            {
+                if (this._personRepository == null)
+                {
+                    this._personRepository = new PersonRepository(this._dbContext);
+                }
+                return _personRepository;
+            }
+        }
 
+        private IContactRepository _contactRepository = null;
+
+        public IContactRepository ContactRepository
+        {
+            get
+            {
+                if (this._contactRepository == null)
+                {
+                    this._contactRepository = new ContactRepository(this._dbContext);
+                }
+                return _contactRepository;
+            }
+        }
+
+        private IAddressRepository _addressRepository = null;
+
+        public IAddressRepository AddressRepository
+        {
+            get
+            {
+                if (this._addressRepository == null)
+                {
+                    this._addressRepository = new AddressRepository(this._dbContext);
+                }
+                return _addressRepository;
+            }
+        }
+        
         private DbContactSystemContext _dbContext;
 
         public UnitOfWork()
         {
             CreateDbContext();
+        }
+
+        public UnitOfWork(DbContactSystemContext dbContext)
+        {
+            this._dbContext = dbContext;
+        }
+        private void CreateDbContext()
+        {
+            this._dbContext = new DbContactSystemContext();
         }
 
         public void Commit()
@@ -32,12 +79,13 @@ namespace Repository
 
         public void Dispose()
         {
-            throw new NotImplementedException();
+            Dispose(true);
+            GC.SuppressFinalize(this);
         }
 
-        private void CreateDbContext()
+        private void Dispose(bool p)
         {
-            _dbContext = new DbContactSystemContext();
+            throw new NotImplementedException();
         }
 
     }
